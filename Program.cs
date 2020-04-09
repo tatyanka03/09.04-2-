130df755dd3be1a2c8dae9d11cf6e481ccc2d838
tatyanka03 <63300782+tatyanka03@users.noreply.github.com>
2020-04-09 09:12:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace itProger2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<short> digits = new List<short>() { 1, 0, 93, 75 };
+            digits.Add(5);
+            digits.AddRange(new short[] { 7, 8, 45 });
+            digits.Insert(1, 178);
+            digits.Remove(0);
+            digits.RemoveAt(2);
+            digits.Sort();
+            Console.WriteLine(digits[2]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
